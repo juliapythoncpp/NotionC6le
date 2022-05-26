daef4fcb720e65c4de86f07b9f2488c97de3477c
Damien Martin <damlobster@gmail.com>
2022-05-26 21:46:52 +0200

    build to dist

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   output: {
     filename: "highlightsToNotion.bundle.js",
-    path: path.resolve(__dirname, "."),
+    path: path.resolve(__dirname, "./dist/"),
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    static: "./dist.browser",
+    static: "./dist",
   },
   output: {
     filename: "highlightsToNotion.bundle.dev.js",
